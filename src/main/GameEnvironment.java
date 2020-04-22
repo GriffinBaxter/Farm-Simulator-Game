c0c37bce304a8c0769dbe3ead657bec4eda126b1
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2020-04-22 23:37:37 +1200

    Added more basic things to GameEnvironment.java

--- src/main/GameEnvironment.java
+++ src/main/GameEnvironment.java
@@ -1,9 +1,32 @@
 package main;
 
-public class GameEnvironment {
+import java.util.Scanner;
 
+public class GameEnvironment {
+	
+	/**
+	 * Starts a new game
+	 */
+	public void startGame() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many days would you like the game to last? (Enter a number between 5 and 10)");
+		String numDays = scanner.nextLine();
+		scanner.close();
+		//Farm();
+	}
+	
+	public void nextDay() {
+		
+	}
+	
+	public void randomEvent() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("SENG 201 Farm Simulator Project - By Griffin Baxter and Rutger van Kruiningen\n");
+		GameEnvironment game = new GameEnvironment();
+		game.startGame();
 
 	}
 
